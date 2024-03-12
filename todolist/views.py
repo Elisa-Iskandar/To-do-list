67478cb9f325a440df196867d4f0e828b8931dd7
Shunto Dohi <112991989+202248SD@users.noreply.github.com>
2024-03-12 10:25:01 +0800

    Update views.py

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'task_list.html', {'tasks': tasks})
+
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'create_task.html')
+
+
+
